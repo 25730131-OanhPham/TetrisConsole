790007e599e5de21d3c255e47dab9652d5431cb3
Noclip <ktxamenbmt@gmail.com>
2026-04-22 19:25:50 +0700

    Fix loop tại game update sleep 500 khiến việc đọc input bị delay
    Theo logic phải tách vòng lặp của tạo block mới với tốc độ refresh của game
    Do đó đã thêm vào 1 biến riêng tại private của Game.h liên quan đến thời điểm rơi block cuối cùng.

--- Game.h
+++ Game.h
@@ -3,13 +3,17 @@
 
 #include "Board.h"
 #include "Block.h"
+#include <chrono>
+
+using namespace std;
 
 class Game {
 private:
     Board board;
     Block currentBlock;
     bool isRunning;
-    
+    // Thêm 1 biến thời gian cho lần rơi block cuối cùng
+    chrono::steady_clock::time_point lastFallTime;
 public:
     Game();
     ~Game();
